12f300726327f6a231a8c38a31605aeb52527d2d
Joe Gesualdo <joegesualdo@gmail.com>
2014-06-07 16:12:23 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ForeverHome::Application.config.secret_key_base = '300771e09ed07e8bf095438d1eeca4ce15f591e6bcb6b4c90a788509ac493be074072261187d182d87b17442066ea93418dde741b8343b1354e9e0e7f6130390'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ForeverHome::Application.config.session_store :cookie_store, key: '_forever-home_session'
